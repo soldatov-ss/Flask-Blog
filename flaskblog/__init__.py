68faf5b30442eb1475dc9bed9f098b31b53fa3df
Soldatov <www.graffity01@gmail.com>
2021-06-08 13:28:38 +0300

    add procfile

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from  flask_sqlalchemy  import  SQLAlchemy
-import os
+from os import urandom, environ
 from flask_ckeditor import CKEditor
 
 app = Flask(__name__)
 CKEditor(app)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///blog.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = environ.get('DATABASE_URL') or "sqlite:///blog.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = os.urandom(20)
+app.config['SECRET_KEY'] = urandom(20)
 db = SQLAlchemy(app)
 db.create_all()
